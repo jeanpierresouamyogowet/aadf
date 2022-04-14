a5dba89f548e935d7f3be8522d9fbd6c02f3d51a
jeanpierresouamyogowet <99475694+jeanpierresouamyogowet@users.noreply.github.com>
2022-04-14 13:31:12 +0200

    Add files via upload

---
+++ aadf-master/index.js
@@ -0,0 +1,9 @@
+const drawerMenu = document.getElementById("drawer-menu");
+
+const openNav = () => {
+  drawerMenu.style.width = "350px";
+};
+
+const closeNav = () => {
+  drawerMenu.style.width = "0px";
+};
